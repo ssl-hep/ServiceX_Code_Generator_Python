809c646f4783d57aa37234782ffdcc0aa95a88e8
Michael-D-Johnson <mdjohnson44@gmail.com>
2022-03-23 18:49:23 -0500

    sorting by oldest first

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -3,7 +3,7 @@ cmd=$2
 path=$3
 while true; do
     if [ -f $path/*.json ]; then
-        for file in `ls $path/*.json`; do
+        for file in `ls -tr $path/*.json`; do
             downloadlink=`grep -o '"file-path":\s.*[Aa-zZ0-9]*' $file |tr -d '"' | tr -d "," | awk '{print $2}'`;
             rm $file;
             command=`$lang $cmd $downloadlink $path`;
